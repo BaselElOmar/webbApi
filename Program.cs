1f205ce47ba66c9e2698bd5ac95df4415e1dcc3c
BaselElOmar <Elomarbasel@gmail.com>
2025-01-21 23:45:33 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+
+app.MapGet("/add", (int num1, int num2) => AddNumbers(num1, num2));
+
+app.MapGet("/subtract", (int num1, int num2) => subtractNumbers(num1, num2));
+
+app.Run();
+
+
+string AddNumbers(int num1, int num2)
+{
+    return $"The sum of {num1} and {num2} is: {num1 + num2}";
+}
+
+string subtractNumbers(int num1, int num2)
+{
+    return $"The dif of {num1} and {num2} is: {num1 - num2}";
+}
